83aa4374b425bcefd2c0469ee07bd7b7a11c63e9
Luiz Paulo <luizpaulo_l@hotmail.com>
2016-07-28 13:49:24 +0000

    signup form concluido (UI)

--- VickProject/VickProject/scripts/App/Controllers/LoginController.js
+++ VickProject/VickProject/scripts/App/Controllers/LoginController.js
@@ -1,3 +1,62 @@
 ﻿app.controller('loginController', function ($scope) {
-    $scope.title = 'Faça seu login';
+    $scope.title = 'Crie aqui seu usuário';
+    $scope.newUser = {
+        userName: '',
+        password: '',
+        confirmPassword: ''
+        
+    };
+
+    $scope.signUp = function () {
+    
+    };
 });
+
+app.directive('password', function ($timeout) {
+    return {
+        require: 'ngModel',
+        controller: function ($element) {
+            var ctrl = $element.controller('ngModel');
+
+            ctrl.$validators.password =
+              function (modelValue, viewValue) {
+                  return viewValue && viewValue.length >= 4
+                    && /[0-9]/.test(viewValue)
+                    && /[a-z]/i.test(viewValue);
+              };
+        }
+    };
+});
+
+app.directive('equals', [
+  function () {
+
+      var link = function ($scope, $element, $attrs, ctrl) {
+
+          var validate = function (viewValue) {
+              var comparisonModel = $attrs.equals;
+
+              if (!viewValue || !comparisonModel) {
+                  ctrl.$setValidity('equals', true);
+              }
+
+              ctrl.$setValidity('equals', viewValue == comparisonModel);
+              return viewValue;
+          };
+
+          ctrl.$parsers.unshift(validate);
+          ctrl.$formatters.push(validate);
+
+          $attrs.$observe('equals', function (comparisonModel) {
+              return validate(ctrl.$viewValue);
+          });
+
+      };
+
+      return {
+          require: 'ngModel',
+          link: link
+      };
+
+  }
+]);
